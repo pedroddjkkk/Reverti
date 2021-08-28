89dd8bee19f14097ad495f8b0777a9522a85168d
pedroddjkkk <cristinakp24@gmail.com>
2021-08-28 18:24:01 -0300

    Update kikodima.js

--- kikodima.js
+++ kikodima.js
@@ -4,3 +4,5 @@ const client = new discord.Client();
 client.on('ready', () => {
     console.log("Kikodima ta on denovo!")
 })
+
+client.login('NzcyNjAyMTE5NTg0MjE5MTY2.X59Dyw.CHjGKdRQcqYLEElJgWneBkHtPbI');
